af4b8a6652100fee7a8d2a323f0f26f134242c71
ldy1993 <994358427@qq.com>
2019-08-27 21:30:54 +0800

    新增学习-activity传值于回传的基本框架2

--- app/src/main/java/com/example/function/study/B_activity.java
+++ app/src/main/java/com/example/function/study/B_activity.java
@@ -11,5 +11,15 @@ public class B_activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b_activity);
+
+        //这里获取到a_activity的数据
+
+        //这里找到textview对象，并且赋值。
+
+        /*************************数据块***********************************/
+        int c=654;
+        String d="a你好呀";
+        /************************************************************/
+        //请传递a，b返回给A_activity
     }
 }
